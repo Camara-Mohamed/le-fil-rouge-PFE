81ad0b7641468b253eb5c103b82781c1a5d1f334
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-05-02 11:17:22 +0200

    refactor(db): run pint

--- app/Http/Requests/VolunteerRequest.php
+++ app/Http/Requests/VolunteerRequest.php
@@ -10,10 +10,10 @@ class VolunteerRequest extends FormRequest
     {
         return [
             'first_name' => ['required', 'string', 'max:255'],
-            'last_name'  => ['required', 'string', 'max:255'],
-            'email'      => ['required', 'email', 'max:255'],
-            'phone'      => ['required', 'string', 'max:20', 'unique:volunteer_requests,phone'],
-            'message'    => ['required', 'string'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'phone' => ['required', 'string', 'max:20', 'unique:volunteer_requests,phone'],
+            'message' => ['required', 'string'],
         ];
     }
 
--- app/Models/Volunteer.php
+++ app/Models/Volunteer.php
@@ -5,8 +5,5 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 
-#[Fillable(['first_name', 'last_name', 'email', 'phone', 'message', 'status',])]
-class Volunteer extends Model
-{
-
-}
+#[Fillable(['first_name', 'last_name', 'email', 'phone', 'message', 'status'])]
+class Volunteer extends Model {}
