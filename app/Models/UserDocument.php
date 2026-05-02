365b295c38f1d85b105b1733f98a37dc2aaa893d
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-05-02 14:59:29 +0200

    feat(db): user document model

--- app/Models/UserDocument.php
+++ app/Models/UserDocument.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
+#[Fillable([
+    'type',
+    'label',
+    'path',
+    'user_id'
+])]
 class UserDocument extends Model
 {
     use SoftDeletes;
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
