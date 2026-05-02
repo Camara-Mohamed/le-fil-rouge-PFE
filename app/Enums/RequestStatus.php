6affeeea365938b637c3281297f4dcfb2e77ffad
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-05-02 13:07:25 +0200

    feat(db): pivot formationUser migration + generate some relative files

--- app/Enums/RequestStatus.php
+++ app/Enums/RequestStatus.php
@@ -2,7 +2,7 @@
 
 namespace App\Enums;
 
-enum VolunteerRequestStatus: string
+enum RequestStatus: string
 {
     case Pending = 'pending';
     case Accepted = 'accepted';
---
+++ app/Policies/FormationUserPolicy.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\FormationUser;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class FormationUserPolicy
+{
+    use HandlesAuthorization;
+
+    public function viewAny(User $user): bool
+    {
+
+    }
+
+    public function view(User $user, FormationUser $formationUser): bool
+    {
+    }
+
+    public function create(User $user): bool
+    {
+    }
+
+    public function update(User $user, FormationUser $formationUser): bool
+    {
+    }
+
+    public function delete(User $user, FormationUser $formationUser): bool
+    {
+    }
+
+    public function restore(User $user, FormationUser $formationUser): bool
+    {
+    }
+
+    public function forceDelete(User $user, FormationUser $formationUser): bool
+    {
+    }
+}
