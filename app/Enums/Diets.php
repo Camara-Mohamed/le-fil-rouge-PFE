ea810b83fb238c5414bee8be0f21d5d96883e465
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-05-07 18:34:40 +0200

    feat(db) : add diets enum

---
+++ app/Enums/Diets.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+enum Diets: string
+{
+    case NORMAL = 'normal';
+    case VEGETARIAN = 'vegetarian';
+    case VEGAN = 'vegan';
+    case HALAL = 'halal';
+    case KOSHER = 'kosher';
+    case GLUTEN_FREE = 'gluten_free';
+    case LACTOSE_FREE = 'lactose_free';
+    case OTHER = 'other';
+}
