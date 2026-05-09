905e4bf6b4b67d051ba7d4f898002488ad88a4f0
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-05-09 12:58:04 +0200

    feat(admin) : implement gates

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Enums\UserRoles;
+use App\Models\User;
+use Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,40 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('access-dashboard', function (User $user): bool {
+            return true;
+        });
+
+        Gate::define('create-training', function (User $user): bool {
+            return $user->hasAnyRole([
+                UserRoles::FORMATEUR,
+                UserRoles::ADMIN,
+            ]);
+        });
+
+        Gate::define('create-camp', function (User $user): bool {
+            return $user->hasAnyRole([
+                UserRoles::COORDINATEUR,
+                UserRoles::ADMIN,
+            ]);
+        });
+
+        Gate::define('create-announcement', function (User $user): bool {
+            return $user->hasAnyRole([
+                UserRoles::ADMIN,
+            ]);
+        });
+
+        Gate::define('create-member', function (User $user): bool {
+            return $user->isAdmin();
+        });
+
+        Gate::define('manage-members', function (User $user): bool {
+            return $user->isAdmin();
+        });
+
+        Gate::define('manage-messages', function (User $user): bool {
+            return $user->isAdmin();
+        });
     }
 }
