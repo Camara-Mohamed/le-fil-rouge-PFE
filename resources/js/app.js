8dbb2338e768c1e4ecace8840d8490b388222c68
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-05-27 16:45:52 +0200

    fix(fancybox): reinstall fancybox

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,7 @@
 import Alpine from 'alpinejs'
-import { Fancybox } from '@fancyapps/ui'
-import '@fancyapps/ui/dist/fancybox/fancybox.css'
+import { Fancybox } from './fancybox.js'
 
 window.Alpine = Alpine
 
 Alpine.start()
-
-Fancybox.bind('[data-fancybox]')
+Fancybox();
---
+++ resources/js/fancybox.js
@@ -0,0 +1,16 @@
+import {Fancybox} from '@fancyapps/ui'
+import '@fancyapps/ui/dist/fancybox/fancybox.css'
+
+export function Fancybox() {
+    Fancybox.bind('[data-fancybox]')
+
+    document.addEventListener('livewire:init', () => {
+        document.addEventListener('livewire:navigated', () => {
+            Fancybox.bind('[data-fancybox]')
+        })
+
+        document.addEventListener('livewire:updated', () => {
+            Fancybox.bind('[data-fancybox]')
+        })
+    })
+}
