4bdcbadba5ece7507ab6d826c7be2da3863663a3
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-05-29 16:02:17 +0200

    feat(dashboard): add cta

--- resources/views/pages/⚡dashboard/dashboard.blade.php
+++ resources/views/pages/⚡dashboard/dashboard.blade.php
@@ -1,5 +1,32 @@
-<p>Bonjour {{ auth()->user()->fullName() }}, tu as {{ auth()->user()->getAge() }} ans ?</p>
+<div class="flex flex-col gap-8 p-8">
 
-// Calendrier
+    <p>Bonjour {{ auth()->user()->fullName() }}, tu as {{ auth()->user()->getAge() }} ans</p>
 
-// CTA's (by Role)
+    <section class="flex flex-col gap-4">
+        <h2>CTA</h2>
+        <div class="flex flex-wrap gap-3">
+
+            @can('manage-members')
+                <a href="{{ route('admin.members.create', ['locale' => app()->getLocale()]) }}">Ajouter un membre</a>
+                <a href="{{ route('admin.members.index', ['locale' => app()->getLocale()]) }}">Gérer les membres</a>
+            @endcan
+
+            @can('manage-messages')
+                <a href="{{ route('admin.messages.index', ['locale' => app()->getLocale()]) }}">Voir les messages</a>
+            @endcan
+
+            @can('manage-training')
+                <a href="{{ route('admin.trainings.create', ['locale' => app()->getLocale()]) }}">Créer une formation</a>
+            @endcan
+
+            @can('manage-camp')
+                <a href="{{ route('admin.camps.create', ['locale' => app()->getLocale()]) }}">Créer un camp</a>
+            @endcan
+
+            @can('manage-announcement')
+                <a href="{{ route('admin.announcements.create', ['locale' => app()->getLocale()]) }}">Ajouter une
+                    actualité</a>
+            @endcan
+        </div>
+    </section>
+</div>
