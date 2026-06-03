cfd9fa8b7e991e3be20c9b792679a2ea5a7659ac
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-06-03 20:10:08 +0200

    style(nav): style skip link

--- lang/fr/general.php
+++ lang/fr/general.php
@@ -1,7 +1,11 @@
 <?php
 
 return [
+    // Skip la navigation
+    'skip_link' => 'Aller au contenu principal',
+
     // Changement de langue
+    'secondary_nav' => 'Navigation secondaire',
     'switch_language' => 'Changer de langue en :language',
     'locales' => [
         'fr' => 'français',
--- resources/views/components/nav/before-nav.blade.php
+++ resources/views/components/nav/before-nav.blade.php
@@ -1,5 +1,5 @@
-<nav class="flex justify-end items-center bg-dark py-3 px-8">
-    <h3 class="sr-only">Navigation secondaire</h3>
+<nav aria-label="{{ __('general.secondary_nav') }}" class="flex justify-end items-center bg-dark py-3 px-8">
+    <h3 class="sr-only">{{ __('general.secondary_nav') }}</h3>
 
     <x-nav.lang-switch></x-nav.lang-switch>
 </nav>
