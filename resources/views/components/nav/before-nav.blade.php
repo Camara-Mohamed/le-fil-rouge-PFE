0f7f8a14aa69de281b8e303ef3a61120134f96ec
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-06-18 00:16:24 +0200

    fix(help): comment help

--- resources/views/components/nav/before-nav.blade.php
+++ resources/views/components/nav/before-nav.blade.php
@@ -1,13 +1,14 @@
 <nav aria-label="{{ __('general.secondary_nav') }}" class="relative z-[110] flex items-center bg-dark py-3 px-4 md:px-6 lg:px-8">
     <h3 class="sr-only">{{ __('general.secondary_nav') }}</h3>
 
-    @auth
+    <!-- @auth
         <a href="{{ route('admin.help', ['locale' => app()->getLocale()]) }}"
            wire:navigate
            class="font-sans text-sm text-dark-light hover:text-white transition duration-200">
             {{ __('pages/help.nav') }}
         </a>
-    @endauth
+    @endauth -->
+
 
     <x-nav.lang-switch></x-nav.lang-switch>
 </nav>
