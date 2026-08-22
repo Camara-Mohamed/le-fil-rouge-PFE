5acdc7c75456ffc9ab1d4d67492ab87c89e10272
Camara Mohamed <mohamed.camara1@student.hepl.be>
2026-08-22 21:33:05 +0200

    feat(trainings): translation files

---
+++ lang/en/pages/trainings.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+    // Sections
+    'section_general' => 'General information',
+    'section_content' => 'Content',
+    'section_dates' => 'Dates',
+    'section_location' => 'Location',
+    'section_roles' => 'Roles allowed to register',
+    'section_images' => 'Images',
+
+    // Fields
+    'label_title' => 'Title',
+    'label_description' => 'Short description',
+    'label_type' => 'Type',
+    'label_price' => 'Price (€)',
+    'label_participants' => 'Max participants',
+    'label_status' => 'Status',
+    'label_details' => 'Details / Objectives',
+    'label_constraints' => 'Constraints',
+    'label_start_date' => 'Start date',
+    'label_end_date' => 'End date',
+    'label_address' => 'Street',
+    'label_number' => 'Number',
+    'label_postal_code' => 'Postal code',
+    'label_city' => 'City',
+    'label_province' => 'Province',
+
+    // Images
+    'label_banner' => 'Banner',
+    'label_gallery' => 'Gallery',
+    'label_new_gallery' => 'New gallery',
+    'banner_choose' => 'Choose a banner…',
+    'banner_change' => 'Change banner…',
+    'gallery_add' => 'Add images to the gallery…',
+    'image_hint' => 'JPG, PNG, GIF or WEBP - 2 MB max',
+    'gallery_image_hint' => 'JPG, PNG, GIF or WEBP - 2 MB max per image',
+    'gallery_delete' => 'Delete',
+
+    // Buttons
+    'create_btn_submit' => 'Create training',
+    'edit_btn_submit' => 'Update',
+    'delete_btn' => 'Delete training',
+
+];
